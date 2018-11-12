586beae53a8539c25256673db0d3f7955e957cba
David Michaux <michaux.dr@gmail.com>
2018-11-12 14:19:22 -0500

    Add basic views and urls

--- marketapp/urls.py
+++ marketapp/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'marketapp'
+
 urlpatterns = [
-  path('', views.index, name = 'index'),
+  path('', views.home, name = 'home'),
+  path('items/', views.index, name = 'index'),
+  path('<int:item_id>/', views.detail, name = 'detail'),
+  path('<int:user_id>/', views.user_page, name = 'user page')
 ]
--- marketapp/views.py
+++ marketapp/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import User, Item
+
+def home(request):
+  # Home page with sign-up/in, my page link, search form featured items
+  return render(request, './home.html')
 
 def index(request):
-  return HttpResponse("Welcome to the Market - where your trash becomes treasure!")
+  # List, search, filter(price, category, list_date, etc.)
+  master_list = Item.objects.all()
+  context = {'master_list': master_list}
+  return render(request, 'items/index.html', context)
+
+def detail(request, item_id):
+  # Details on item, form to message about it
+  item = get_object_or_404(Item, pk = item_id)
+  context = {'item': item}
+  return render(request, 'items/detail.html', context)
+
+def user_page(request, user_id):
+  # User info, messages, listed items, favorited items?
+  user = get_object_or_404(User, pk = user_id)
+  context = {'user': user}
+  return render(request, 'users/detail.html', context)
