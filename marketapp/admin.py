0a3d8d88d5b30a2f9af08e9bb9c4dbec0df71e33
David Michaux <michaux.dr@gmail.com>
2018-11-13 15:24:38 -0500

    Add Category model to admin

--- marketapp/admin.py
+++ marketapp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import User, Item, Message
+from .models import User, Category, Item, Message
 
 admin_models = [
   User,
+  Category,
   Item,
   Message
 ]
